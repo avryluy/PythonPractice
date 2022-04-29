1badf3ad205fcd698c37936d52890967715a9209
Avry Luy <avryluy@mailbox.org>
2022-04-29 14:26:00 -0500

    Added random message response with time delay

---
+++ magic8ball.py
@@ -0,0 +1,27 @@
+import random, time
+def getanswer (answerNumber):
+    if answerNumber == 1:
+        return 'It is certain.'
+    elif answerNumber == 2:
+        return 'It is decidedly so.'    
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy, try again'    
+    elif answerNumber == 5:
+        return 'Ask again'    
+    elif answerNumber == 6:
+        return 'Concentrate... '    
+    elif answerNumber == 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Outlook not so good'        
+    elif answerNumber == 9:
+        return 'Very doubtful.'    
+r = random.randint(1, 9)   
+print(r)     
+time.sleep(.5)
+print('Deciding...')
+time.sleep(2)
+fortune = getanswer(r)
+print(fortune)

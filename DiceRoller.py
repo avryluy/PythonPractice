16e2704f6c2a290f2e3999ead2bbb9f212595d06
Avry Luy <avryluy@mailbox.org>
2022-04-29 17:45:10 -0500

    confirmed output

--- DiceRoller.py
+++ DiceRoller.py
@@ -4,3 +4,7 @@ print('How many dice are you rolling?')
 dicenum = input()
 print('What sided die are you rolling (ex. d6, d8)')
 diceside = input()
+
+
+
+print(f'You want to roll a {int(dicenum)}d{int(diceside)}')
